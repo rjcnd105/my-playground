5aa241a63ffef90e6c4744f1d4b1087b32bef47d
hoejun imac <rjcnd123@gmail.com>
2020-02-28 01:13:23 +0900

    함수 만듦

---
+++ src/utils/equals.test.ts
@@ -0,0 +1,7 @@
+import { eqString } from 'fp-ts/lib/Eq'
+
+it('should eqString', function() {
+  eqString.equals('a', 'a')
+
+  // expect(e)
+})
---
+++ src/utils/equals.ts
@@ -0,0 +1,3 @@
+import { eqString } from 'fp-ts/es6/Eq'
+
+eqString.equals('a', 'a')
