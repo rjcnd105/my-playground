2a9b28e89a2589a5f78e93e8c05b282a200c3e05
hj_macbook <rjcnd123@gmail.com>
2021-07-03 22:30:43 +0900

    storybook setting

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,10 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/preset-create-react-app',
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      loader: require.resolve('babel-loader'),
-    })
-    config.resolve.extensions.push('.ts', '.tsx')
-
-    return config
-  },
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
 }
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
-// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Meta, Story } from '@storybook/react/types-6-0'
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Button, ButtonProps } from './Button'
+import { Button } from './Button';
 
 export default {
   title: 'Example/Button',
@@ -10,29 +9,29 @@ export default {
   argTypes: {
     backgroundColor: { control: 'color' },
   },
-} as Meta
+} as ComponentMeta<typeof Button>;
 
-const Template: Story<ButtonProps> = (args) => <Button {...args} />
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({})
+export const Primary = Template.bind({});
 Primary.args = {
   primary: true,
   label: 'Button',
-}
+};
 
-export const Secondary = Template.bind({})
+export const Secondary = Template.bind({});
 Secondary.args = {
   label: 'Button',
-}
+};
 
-export const Large = Template.bind({})
+export const Large = Template.bind({});
 Large.args = {
   size: 'large',
   label: 'Button',
-}
+};
 
-export const Small = Template.bind({})
+export const Small = Template.bind({});
 Small.args = {
   size: 'small',
   label: 'Button',
-}
+};
