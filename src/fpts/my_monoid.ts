bc3ffbe1ff42b21c63087f2262f9f57c6c0e49de
hj <rjcnd123@gmail.com>
2021-07-16 10:09:27 +0900

    monoid2

--- src/fpts/my_monoid.ts
+++ src/fpts/my_monoid.ts
@@ -2,8 +2,8 @@
  * Monoid
  * semigroup에서 empty가 추가된 개념.
  *
- * Right identity: concat(x, empty) = x
- * Left identity: concat(empty, x) = x
+ * Right identity(오른쪽 항등식): concat(x, empty) = x
+ * Left identity(왼쪽 항등식): concat(empty, x) = x
  * 그렇기 위해서는 항등원이 필요함.
  *
  * Monoid에서는 항등원(empty)를 넣으면 위의 identity가 지켜진다는 것이 보장되므로,
@@ -74,10 +74,12 @@ interface User {
   readonly age: number
   readonly rememberMe: boolean
 }
+
 const byName = pipe(
   S.Ord,
   contramap((p: User) => p.name)
 )
+
 const byId = pipe(
   N.Ord,
   contramap((p: User) => p.id)
@@ -102,11 +104,27 @@ const users: Array<User> = [
   { id: 4, name: 'Giulio', age: 44, rememberMe: true },
 ]
 
-// 왜?????
-const ord1 = concatAll(M)([byId])
-const ord11 = concatAll(M)([byRememberMe, byId])
-sort(ord1)(users) /*?*/
+// 넣는 순서에 따라 1순위 2순위 정렬이 달라짐... 왜?????
+// 결합법칙에 위배되지 않나???
+// const ord1 = concatAll(M)([byId])
+const ord11 = M.concat(byRememberMe, byId)
+const ord12 = M.concat(byId, byRememberMe)
+// sort(ord1)(users) /*?*/
 sort(ord11)(users) /*?*/
+/*
+* [ { id: 1, name: 'Guido', age: 47, rememberMe: false },
+  { id: 3, name: 'Giulio', age: 44, rememberMe: false },
+  { id: 2, name: 'Guido', age: 46, rememberMe: true },
+  { id: 4, name: 'Giulio', age: 44, rememberMe: true } ]
+  * */
+
+sort(ord12)(users) /*?*/
+/*
+* [ { id: 1, name: 'Guido', age: 47, rememberMe: false },
+  { id: 2, name: 'Guido', age: 46, rememberMe: true },
+  { id: 3, name: 'Giulio', age: 44, rememberMe: false },
+  { id: 4, name: 'Giulio', age: 44, rememberMe: true } ]
+  * */
 
 // sort by name, then by age, then by `rememberMe`
 const ord2 = concatAll(M)([byName, byAge, byRememberMe])
