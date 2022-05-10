e3e563246902da9f77073e5e1cd44a8a188363cd
hjm1 <rjcnd123@gmail.com>
2022-05-10 17:03:02 +0900

    다시 해보니까 뭔소린지 몰겠다..

--- src/fpts/testroom.ts
+++ src/fpts/testroom.ts
@@ -3,7 +3,8 @@ import * as RT from 'fp-ts/lib/ReaderTask'
 import * as T from 'fp-ts/lib/Task'
 import * as TE from 'fp-ts/lib/TaskEither'
 import * as IO from 'fp-ts/lib/IO'
-import { pipe } from 'fp-ts/lib/function'
+import * as E from 'fp-ts/lib/Either'
+import { flow, pipe } from 'fp-ts/lib/function'
 import axios, { AxiosResponse } from 'axios'
 
 interface ApiError {
@@ -17,18 +18,50 @@ interface Todo {
   title: string,
   completed: boolean
 }
-await axios.get('https://jsonplaceholder.typicode.com/todos/1') /*?*/
+// await axios.get('https://jsonplaceholder.typicode.com/todos/1') /*?*/
 
 // https://jsonplaceholder.typicode.com/todos/1
 
-const getTodo: RTE.ReaderTaskEither<string, ApiError, AxiosResponse<Todo>> =
-  RTE.asks((todoId) =>
-    TE.tryCatch(
-      () => axios.get(`https://jsonplaceholder.typicode.com/todos/${todoId}`),
-      () => {}
-    )
-  )
+// flow(
+//   TE.tryCatch(
+//   () => axios.get(`https://jsonplaceholder.typicode.com/todos/1`),
+//   (err) => err
+//   ),
+//   TE.match(() => )
+// ) /*?*/
 
+// const getUserExecute: RT.ReaderTask<Todo['userId'], never> = (todoId) =>
+
+const f = () => {
+    return axios.get(`https://jsonplaceholder.typicode.com/todos/1`)
+  }
+const getTodo: RTE.ReaderTaskEither<
+  string,
+  ApiError,
+  AxiosResponse<Todo>
+> = (todoId) =>
+    pipe(TE.tryCatch(
+      () => {
+        console.log(todoId)
+        return axios.get(`https://jsonplaceholder.typicode.com/todos/${todoId}`)
+      },
+      (r) => {
+        console.log(r)
+        return {
+          code: 333,
+          message: 'fail',
+        } as ApiError
+      }
+    ),
+        TE.map(res => res.data),
+
+    )
+
+const myTask1 = getTodo('1')() /*?*/
+
+
+// await getTodo('3')() /*?*/
+ 
 
 function t1() {
   pipe(RTE)
