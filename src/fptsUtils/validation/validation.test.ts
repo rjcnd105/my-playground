ed4e2da1d4d0169e540d854029e3fe7602c18fe6
hjm1 <rjcnd123@gmail.com>
2022-06-13 19:12:16 +0900

    validation 수정

--- src/fptsUtils/validation/validation.test.ts
+++ src/fptsUtils/validation/validation.test.ts
@@ -8,31 +8,34 @@ const validNameLength = validator(
 )
 const validNameAA = validator((name: string) => name.includes('aa'),  'aa가 2개 이상 들어가 있어야 합니다.')
 
-validNameLength("#test")(right('aaaa')) /*?*/
-validNameLength("#test")(right('asdfsdfsdfds')) /*?*/
 
-const my_validation = pipe(right(''), validNameLength("#test"), validNameAA("#test")) /*?*/
-
-test('"aaaa" 길이 밸리데이션', () => {
-  expect(validNameLength("#test")(right('aaaa'))).toStrictEqual(right('aaaa'))
-})
-test('"열글자에요열글자에요" 길이 밸리데이션', () => {
-  expect(isLeft(validNameLength("#test")(right('열글자에요열글자에요')))).toBe(true)
-})
-test('빈 문자열 밸리데이션 2개', () => {
-  expect(my_validation).toStrictEqual(
-    left({
-      value: '',
-      errors: [
-        {
-          identifier: '#test',
-          message: '이름은 1~9 글자로 지어야 합니다.',
-        },
-        {
-          identifier: '#test',
-          message: 'aa가 2개 이상 들어가 있어야 합니다.',
-        },
-      ],
-    })
-  )
-})
+// // 과거 테스트들
+//
+// validNameLength("#test")(right('aaaa')) /*?*/
+// validNameLength("#test")(right('asdfsdfsdfds')) /*?*/
+//
+// const my_validation = pipe(right(''), validNameLength("#test"), validNameAA("#test")) /*?*/
+//
+// test('"aaaa" 길이 밸리데이션', () => {
+//   expect(validNameLength("#test")(right('aaaa'))).toStrictEqual(right('aaaa'))
+// })
+// test('"열글자에요열글자에요" 길이 밸리데이션', () => {
+//   expect(isLeft(validNameLength("#test")(right('열글자에요열글자에요')))).toBe(true)
+// })
+// test('빈 문자열 밸리데이션 2개', () => {
+//   expect(my_validation).toStrictEqual(
+//     left({
+//       value: '',
+//       errors: [
+//         {
+//           identifier: '#test',
+//           message: '이름은 1~9 글자로 지어야 합니다.',
+//         },
+//         {
+//           identifier: '#test',
+//           message: 'aa가 2개 이상 들어가 있어야 합니다.',
+//         },
+//       ],
+//     })
+//   )
+// })
--- src/fptsUtils/validation/validation.ts
+++ src/fptsUtils/validation/validation.ts
@@ -1,62 +1,40 @@
+import { chain } from 'fp-ts/es6/Either'
+import { ap, sequenceS } from 'fp-ts/lib/Apply'
 import type { Either } from 'fp-ts/lib/Either'
-import { fromPredicate, isLeft, left, match, right } from 'fp-ts/lib/Either'
-import { pipe } from 'fp-ts/lib/function'
+import * as E from 'fp-ts/lib/Either'
+import Endo from 'fp-ts/lib/Endomorphism'
+import { flow, pipe } from 'fp-ts/lib/function'
+import M from 'fp-ts/lib/Monoid'
+import type { NonEmptyArray } from 'fp-ts/lib/NonEmptyArray'
+import { concat } from 'fp-ts/lib/NonEmptyArray'
 import type { Predicate } from 'fp-ts/lib/Predicate'
+import type { Reader } from 'fp-ts/lib/Reader'
+import * as RE from 'fp-ts/lib/ReaderEither'
+import * as ReadonlyArr from 'fp-ts/lib/ReadonlyArray'
+import { matchE } from 'fp-ts/ReaderEither'
 
-type Identifier = string
-type Message = string
+export type Message = string
 
-export interface ValidationError {
-  readonly identifier: Identifier
-  readonly message?: Message
+export type ValidationErrors = {
+  readonly value: unknown
+  readonly errors: NonEmptyArray<Message>
 }
 
-export type ValidationErrors<A> = {
-  value: A
-  errors: ValidationError[]
-}
-
-export type ValidationErrorMap<A> = {
-  value: A
-  errorMap: Map<Identifier, Message>
-}
-
-export type Validation<A> = Either<ValidationErrors<A>, A>
-export type ValidationMap<A> = Either<ValidationErrorMap<A>, A>
+export type Validation<A> = Either<ValidationErrors, A>
 
 // 모든 에러 검출시 사용
-export const validator =
-  <A>(pred: Predicate<A>, errorMessage: Message) =>
-    (identifier: Identifier) =>
-      match<ValidationErrors<A>, A, Validation<A>>(
-        ({ value, errors }) =>
-          pred(value)
-            ? left({ value, errors })
-            : left({
-              value,
-              errors: errors.concat({ message: errorMessage, identifier }),
-            }),
-        fromPredicate(pred, value => ({
-          value,
-          errors: [{ message: errorMessage, identifier }],
-        })),
-      )
+export const validator = <A>(pred: Predicate<A>, errorMessage: Message) =>
+  E.match<ValidationErrors, A, Validation<A>>(
+    left => E.left(pred(left.value as A) ? left : { value: left.value, errors: concat(left.errors)([errorMessage]) }),
+    value => (pred(value) ? E.right(value) : E.left({ value, errors: [errorMessage] })),
+  )
 
-// 각 필드당 하나의 에러를 검출할 때
+// 각 identifier당 하나의 에러를 검출할 때
 // 일반적인 UI에 쉽게 사용하기 위한 것
-export const mapValidator =
-  <A>(pred: Predicate<A>, errorMessage: Message) =>
-    (identifier: Identifier) =>
-      match<ValidationErrorMap<A>, A, ValidationMap<A>>(
-        ({ value, errorMap }) =>
-          errorMap.has(identifier) || pred(value)
-            ? left({ value, errorMap })
-            : left({
-              value,
-              errorMap: errorMap.set(identifier, errorMessage),
-            }),
-        fromPredicate(pred, value => ({
-          value,
-          errorMap: new Map([[identifier, errorMessage]]),
-        })),
-      )
+export const singleErrorValidator = <A>(pred: Predicate<A>, errorMessage: Message) =>
+  E.chain<Message, A, A>(value => (pred(value) ? E.right(value) : E.left(errorMessage)))
+
+// validation을 하기 위해 value로부터 검증 가능한 Either 모나드로 끌어올림
+export const liftE = <A>(v: A) => E.of(v)
+// struct 형태에 대한 전체 검증
+export const validatorS = sequenceS(E.Apply)
