ed1344615b819c6e2538152b85d7ea2f78e6353b
hjm1 <rjcnd123@gmail.com>
2022-06-23 19:21:25 +0900

    메세지 헬프 함수

--- src/models/Message.ts
+++ src/models/Message.ts
@@ -18,3 +18,11 @@ export const isMessage = (o: unknown): o is Message => {
   }
   return false
 }
+
+export const isErrorMessage = (o: unknown): o is Omit<Message, 'status'> & { status: 'error' } => {
+  return isMessage(o) && o.status === 'error'
+}
+
+export const isSuccessMessage = (o: unknown): o is Omit<Message, 'status'> & { status: 'success' } => {
+  return isMessage(o) && o.status === 'success'
+}
